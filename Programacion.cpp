b08c37de91445cd847fdb9749a72bc307517155e
Sedeperior <sdemianpr@gmail.com>
2025-12-02 23:49:33 -0600

    Delete Programacion.cpp

--- Programacion.cpp
+++
@@ -1,9 +0,0 @@
-#include "Programacion.h"
-
-Programacion::Programacion() : Curso("Programacion") {}
-
-std::unique_ptr<Curso> Programacion::clone() const {
-    auto copia = std::make_unique<Programacion>();
-    copia->setCalificacionFinal(this->getCalificacionFinal());
-    return copia;
-}

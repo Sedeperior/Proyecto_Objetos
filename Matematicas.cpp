ae1b40a9fe9ade5b955da2ab42afc8cb13c26806
Sedeperior <sdemianpr@gmail.com>
2025-12-02 23:48:44 -0600

    Delete Matematicas.cpp

--- Matematicas.cpp
+++
@@ -1,9 +0,0 @@
-#include "Matematicas.h"
-
-Matematicas::Matematicas() : Curso("Matematicas") {}
-
-std::unique_ptr<Curso> Matematicas::clone() const {
-    auto copia = std::make_unique<Matematicas>();
-    copia->setCalificacionFinal(this->getCalificacionFinal());
-    return copia;
-}
